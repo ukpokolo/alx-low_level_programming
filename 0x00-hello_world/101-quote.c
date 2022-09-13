9d8e29f1e444d03ea9af26c1407aca7ff371bfca
Irenose <jiukpokolo@gmail.com>
2022-09-12 18:22:51 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   * Return:1
+ * Return: Always 1 (Success)
  *
- *    */
-
+ */
 int main(void)
-
 {
-
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, ch, sizeof(ch));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
